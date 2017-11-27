3db6cb5081bc0a64334983cc7b99ef38da0da320
Arturo Filio Villa <arturfil@hotmail.com>
2017-11-27 17:03:18 -0500

    added switch statement to reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,7 +3,25 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                counter: state.counter + 1
+            }
+        case 'DECREMENT':
+            return {
+                counter: state.counter - 1
+            }
+        case 'ADD':
+            return {
+                counter: state.counter + action.val
+            }
+        case 'SUBSTRACT':
+            return {
+                counter: state.counter + action.val
+            }
+    };
     return state;
-};
+}
 
 export default reducer;
